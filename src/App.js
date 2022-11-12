e6a22f73f0d4a225cd395b810f05619e260e4168
Yhucee14 <uchennachukwu2002@yahoo.com>
2022-11-11 21:31:22 -0800

    Installed MUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Container from './Components/Container';
 
 function App() {
   return (
     <div className="App">
-     Welcome
+     <Container />
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../Header/House.svg';
+import {AppBar, Toolbar} from '@mui/material';
+
+
+const Header = () => {
+  return (
+    <div>
+        <img src={logo} className="App-logo" alt="logo" c/>
+    </div>
+  )
+}
+
+export default Header
