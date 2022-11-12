12bf152fabf5416b61cb89415c4c6068feec86a6
Yhucee14 <uchennachukwu2002@yahoo.com>
2022-11-12 02:04:37 -0800

    Setup the navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,72 @@
 import React from 'react'
 import logo from '../Header/House.svg';
-import {AppBar, Toolbar} from '@mui/material';
+import name from '../Header/name.svg';
+import {AppBar, Toolbar, IconButton, Stack, Button} from '@mui/material';
+import { ThemeProvider, createMuiTheme } from '@mui/material/styles';
+
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Red Rose',
+        '"Segoe UI Symbol"',
+      ].join(','),
+
+      fontSize: 20,
+      fontWeight: 400,
+     
+      button: {
+        textTransform: 'none'
+      }
+    },
+
+  });
+  
+
 
 
 const Header = () => {
   return (
-    <div>
-        <img src={logo} className="App-logo" alt="logo" c/>
-    </div>
+    <ThemeProvider theme={theme}>
+    <AppBar position='static' sx={{
+        padding: 1,
+        marginTop: 3,
+        background: 'transparent', 
+        boxShadow: 'none'
+    
+      }}>
+        <Toolbar sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+    
+      }}>
+            <IconButton size='large' edge='start' aria-label='logo' style={{marginTop: -10}}>
+            <img src={logo} alt="logo" /> 
+            <img src={name} alt="name" style={{
+           marginTop: 6,
+           padding: 3
+        }}/>
+            </IconButton>
+
+
+            <Stack direction='row' spacing='2' style={{padding: 3}}>
+                <Button sx={{ 
+                    color: 'text.primary', paddingRight: 5, paddingLeft: 5}}>Home</Button>
+                <Button sx={{ color: 'text.primary', paddingRight: 5, paddingLeft: 5 }}>Place to stay</Button>
+                <Button sx={{ color: 'text.primary', paddingRight: 5, paddingLeft: 5 }}>NFTs</Button>
+                <Button sx={{ color: 'text.primary', paddingRight: 5, paddingLeft: 5 }}>Community</Button>
+            </Stack>
+
+            <Button variant='contained' sx={{
+                bgcolor: '#A02279',
+                borderRadius: 2
+            }}>
+                Connect wallet
+            </Button>
+          
+        </Toolbar>
+      
+    </AppBar>
+    </ThemeProvider>
   )
 }
 
